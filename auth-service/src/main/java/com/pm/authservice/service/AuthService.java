bd12ac76039ad68928f0a86831eacf951587eb5d
HaoHuang <huang.hao8@northeastern.edu>
2025-10-12 00:18:15 -0400

    add part of auth service

---
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -0,0 +1,46 @@
+package com.pm.authservice.service;
+
+import com.pm.authservice.dto.LoginRequestDTO;
+import com.pm.authservice.model.User;
+import com.pm.authservice.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+    }
+
+    //password request -> password -> encoded -> $2b$12$7hoRZfJrRKD2nIm2vHLs7OBETy.LWenXXMLKf99W8M4PUwO6KB7fu
+    public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
+        Optional<String> token = userService.findByEmail(loginRequestDTO.getEmail())//get the user
+                .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(),
+                        u.getPassword()))//pass the user to filter function
+                .map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));//generate token
+
+        return token;
+
+    }
+
+//    public boolean validateToken(String token) {
+//        try{
+//            jwtUtil.validateToken(token);
+//            return true;
+//        }catch (JwtException e){
+//            return false;
+//        }
+//
+//    }
+}
